e110e165579a634846d7017d192b6dbc42462e56
legoman77 <rplegos@gmail.com>
2014-07-12 11:05:23 -0400

    added days

--- hellopeeps.py
+++ hellopeeps.py
@@ -1,7 +1,7 @@
-your_name = input("What's yo name bro?\n ")
+your_name = input("What's yo name bro?\n ").strip().title()
 print("Yo yo " +  your_name + "!")
 
-other_name = input("What's yo bro's name?\n ")
+other_name = input("What's yo bro's name?\n ").strip().title()
 print("Yo yo " + other_name + "!")
 
 your_age = input("What's yo age?\n ")
@@ -13,6 +13,8 @@ other_age = int(other_age)
 years_apart = (your_age - other_age)
 years_apart = abs(years_apart)
 
-print("You are " + str(years_apart) + " years apart ")
+days_apart = years_apart * 365.242
+
+print("You are {} years apart or {} days apart".format(years_apart,days_apart))
 
 
