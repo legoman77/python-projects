056d794b17b1727b885a2c1e87698bd3b94b54c1
legoman77 <rplegos@gmail.com>
2014-09-13 10:57:12 -0400

    battleship

---
+++ battleship.py
@@ -0,0 +1,31 @@
+print("Prepare for battle!")
+
+w = '~'
+hit = '*'
+
+
+
+
+grid = [
+    [' ',0,1,2,3,4,5,6,7,8,9],
+    ['A',w,w,w,w,w,w,w,w,w,w],
+    ['B',w,w,w,w,w,w,w,w,w,w],
+    ['C',w,w,w,w,w,w,w,w,w,w],
+    ['D',w,w,w,w,w,w,w,w,w,w],
+    ['E',w,w,w,w,w,w,w,w,w,w],
+    ['F',w,w,w,w,w,w,w,w,w,w],
+    ['G',w,w,w,w,w,w,w,w,w,w],
+    ['H',w,w,w,w,w,w,w,w,w,w],
+    ['I',w,w,w,w,w,w,w,w,w,w],
+    ['J',w,w,w,w,w,w,w,w,w,w],
+
+
+]
+
+grid[5][5] = hit
+
+for row in grid:
+    for col in row:
+        print(col,end=' ')
+    print(' ')    
+
