3d440f3c49787820d89249722c0337e53ab01de6
legoman77 <rplegos@gmail.com>
2014-10-11 10:34:36 -0400

    ported artist 1

---
+++ code-studio/s1level24.py
@@ -0,0 +1,17 @@
+'''
+moveForward(100);
+turnRight(90);
+moveForward(100);
+'''
+
+
+
+from turtle import Turtle
+
+artist = Turtle()
+artist.pensize(7)
+artist.pencolor('red')
+
+artist.forward(100)
+artist.right(90)
+artist.forward(100)
